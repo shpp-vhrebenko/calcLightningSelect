ccf1f7feb3dd765bcdcc37882b6054f52172c165
shpp-vhrebenko <vhrebenko@gmail.com>
2017-04-09 16:00:47 +0300

    edit view validation

--- scripts/widget/form_validation.js
+++ scripts/widget/form_validation.js
@@ -76,7 +76,7 @@
             customRequiredIllumination:{
                 required: 'Это поле обязательно для заполнения'
             },
-            typeLamp:{
+            nameLamp:{
                 required: 'Это поле обязательно для заполнения'
             },
             luminousFlux:{
@@ -100,101 +100,7 @@
     }    
 
    
-    function editLampFormValidation() {
-        console.log("editLampFormValidation");             
-
-        $.validator.addMethod('greaterThanZero', function(value, element) {
-            return this.optional(element) || (parseFloat(value) > 0);
-        }, '');
-
-        $('#editLamp').validate({
-          debug: true,
-          rules:{
-            widthRoom:{
-                required: true,
-                greaterThanZero: true
-            },
-            lengthRoom:{
-                required: true,
-                greaterThanZero: true
-            },
-            heightRoom:{
-                required: true,
-                greaterThanZero: true
-            },        
-            lampsWorkHeight:{
-                required: true,           
-                greaterThanZero: true
-            },
-            reflectionCoef:{
-                required: true
-            },
-            safetyFactor:{
-                required: true
-            },
-            requiredIllumination: {
-                required: true,
-                greaterThanZero: true
-            },
-            customRequiredIllumination: {
-                required: true,
-                greaterThanZero: true
-            },
-            nameLamp: {
-                required: true
-            },
-            luminousFlux: {
-                required: true,
-                greaterThanZero: true
-            }
-          },
-
-          messages:{
-
-            widthRoom:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            lengthRoom:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            heightRoom:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            heightRoomlighting:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            reflectionCoef:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            safetyFactor:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            requiredIllumination:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            customRequiredIllumination:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            typeLamp:{
-                required: 'Это поле обязательно для заполнения'
-            },
-            luminousFlux:{
-                required: 'Это поле обязательно для заполнения'
-            }
-          },
-            focusInvalid: false,
-            errorPlacement: function (error, element) {
-              return true;
-            }
-        });
-
-        console.info($('#editLamp').valid());
-        if ($('#editLamp').valid()) {
-            $('#saveEdit').prop('disabled', false);            
-        } else {
-            $('#saveEdit').prop('disabled', 'disabled');            
-        }
-    }    
+    
 
 
    
