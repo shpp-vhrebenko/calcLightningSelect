dd3dce3e6eaf8e192317d861eb1e5eb5a1d7354c
shpp-vhrebenko <vhrebenko@gmail.com>
2017-05-05 17:00:40 +0300

    edit post message

--- scripts/widget/post_message.js
+++ scripts/widget/post_message.js
@@ -1,5 +1,29 @@
 //==================POST MESSAGE===================================
 
+ /**
+   * [ listener event 'message' from parent window (apply postMessage)] 
+   */
+  window.addEventListener('message', function(event) {
+    var a = event.data.message;
+    window.location.hash = event.origin;
+    if (a.cmd == 'put_data') {
+      var data = a.data; 
+      console.group("LOAD DATA"); 
+      console.log(typeof(data));
+      console.log(data);   
+      console.groupEnd(); 
+      var parentURL = window.location.hash.slice(1);   
+      current_Room.getInstance().setInstanceParentUrl(parentURL);
+      current_Room.getInstance().setTypeLamp(data);      
+      viewResultInTable(data);
+      /*localDataLamp.jsonDrawing = json_object;
+      localStorage.setItem('typeLamp', JSON.stringify(localDataLamp));*/
+      sendResultForDrawPlan(JSON.stringify(data));           
+    } else {
+      initSelectorNameLamp();   
+    }
+  }, false);
+
 /**
  * [ listener event 'message' from parent window (apply postMessage)] 
  */
